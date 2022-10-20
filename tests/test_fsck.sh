a8b4126be67755dc2abb5bd8f90c75053617ee29
Namjae Jeon <linkinjeon@kernel.org>
2022-10-20 13:14:36 +0900

    ntfsprogs: add paragon test images
    Add the test images that making paragon ntfs corruption.
    
    Signed-off-by: Namjae Jeon <linkinjeon@kernel.org>

---
+++ tests/test_fsck.sh
@@ -0,0 +1,77 @@
+#!/usr/bin/env bash
+
+TESTCASE_DIR=$1
+NEED_LOOPDEV=$2
+IMAGE_FILE=ntfs.img
+FSCK_PROG=ntfsck
+FSCK_PROG_2=ntfsck
+FSCK_OPTS="-a"
+PASS_COUNT=0
+
+cleanup() {
+	echo ""
+	echo "Passed ${PASS_COUNT} of ${TEST_COUNT}"
+	if [ ${PASS_COUNT} -ne ${TEST_COUNT} ]; then
+		exit 1
+	else
+		exit 0
+	fi
+}
+
+if [ $# -eq 0 ]; then
+	TESTCASE_DIRS=$(find . -mindepth 1 -maxdepth 1 -type d)
+	TEST_COUNT=$(find . -mindepth 1 -maxdepth 1 -type d | wc -l)
+else
+	TESTCASE_DIRS=$@
+	TEST_COUNT=$#
+fi
+
+for TESTCASE_DIR in $TESTCASE_DIRS; do
+	if [ ! -e "${TESTCASE_DIR}/${IMAGE_FILE}.tar.xz" ]; then
+		TEST_COUNT=$((TEST_COUNT - 1))
+		continue
+	fi
+
+	echo "Running ${TESTCASE_DIR}"
+	echo "-----------------------------------"
+
+	# Set up image file as loop device
+	tar -C . -xf "${TESTCASE_DIR}/${IMAGE_FILE}.tar.xz"
+	if [ $NEED_LOOPDEV ]; then
+		DEV_FILE=$(losetup -f "${IMAGE_FILE}" --show)
+	else
+		DEV_FILE=$IMAGE_FILE
+	fi
+
+	# Run fsck for repair
+	$FSCK_PROG $FSCK_OPTS "$DEV_FILE"
+	if [ $? -ne 1 ] && [ $? -ne 0 ]; then
+		echo ""
+		echo "Failed to repair ${TESTCASE_DIR}"
+		if [ $NEED_LOOPDEV ]; then
+			losetup -d "${DEV_FILE}"
+		fi
+		cleanup
+	fi
+
+	echo ""
+	# Run fsck again
+	$FSCK_PROG_2 "$DEV_FILE"
+	if [ $? -ne 0 ]; then
+		echo ""
+		echo "Failed, corrupted ${TESTCASE_DIR}"
+		if [ $NEED_LOOPDEV ]; then
+			losetup -d "${DEV_FILE}"
+		fi
+		cleanup
+	fi
+
+	echo ""
+	echo "Passed ${TESTCASE_DIR}"
+	PASS_COUNT=$((PASS_COUNT + 1))
+
+	if [ $NEED_LOOPDEV ]; then
+		losetup -d "${DEV_FILE}"
+	fi
+done
+cleanup
